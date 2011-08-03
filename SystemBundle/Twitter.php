a0a33556692285107e0567cbe67e2e5a3153f2be
Fernando Santos <o@fernan.do>
2011-08-03 06:51:30 -0300

    Adicionado serviço para Twitter Oauth

---
+++ SystemBundle/Twitter.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Mastop\SystemBundle;
+
+use Symfony\Component\HttpFoundation\Session;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use \TwitterOAuth;
+
+class Twitter {
+
+    private $twitter;
+    private $callbackURL;
+
+    public function __construct(TwitterOAuth $twitter, $callbackURL = null)
+    {
+        $this->twitter = $twitter;
+        $this->callbackURL = $callbackURL;
+    }
+
+    public function getLoginUrl(Request $request)
+    {
+        $session = $request->getSession();
+
+        /* Get temporary credentials. */
+        $requestToken = $this->callbackURL ? $this->twitter->getRequestToken($this->callbackURL) : $this->twitter->getRequestToken();
+
+        /* Save temporary credentials to session. */
+        $session->set('oauth_token', $requestToken['oauth_token']);
+        $session->set('oauth_token_secret', $requestToken['oauth_token_secret']);
+
+        /* If last connection failed don't display authorization link. */
+        switch ($this->twitter->http_code) {
+            case 200:
+                /* Build authorize URL and redirect user to Twitter. */
+                $redirectURL = $this->twitter->getAuthorizeURL($requestToken);
+                return $redirectURL;
+                break;
+            default:
+                /* return null if something went wrong. */
+                return null;
+        }
+    }
+
+    public function getAccessToken(Request $request)
+    {
+        $session = $request->getSession();
+
+        //set OAuth token in the API
+        $this->twitter->setOAuthToken($request->get('oauth_token'), $session->get('oauth_token_secret'));
+
+        /* Check if the oauth_token is old */
+        if ($session->has('oauth_token')) {
+            if ($session->get('oauth_token') && ($session->get('oauth_token') !== $request->get('oauth_token'))) {
+                $session->remove('oauth_token');
+                return null;
+            }
+        }
+
+        /* Request access tokens from twitter */
+        $accessToken = $this->twitter->getAccessToken($request->get('oauth_verifier'));
+
+        /* Save the access tokens. Normally these would be saved in a database for future use. */
+        $session->set('access_token', $accessToken['oauth_token']);
+        $session->set('access_token_secret', $accessToken['oauth_token_secret']);
+
+        /* Remove no longer needed request tokens */
+        !$session->has('oauth_token') ?: $session->remove('oauth_token', null);
+        !$session->has('oauth_token_secret') ?: $session->remove('oauth_token_secret', null);
+
+        /* If HTTP response is 200 continue otherwise send to connect page to retry */
+        if (200 == $this->twitter->http_code) {
+            /* The user has been verified and the access tokens can be saved for future use */
+            return $accessToken;
+        }
+
+        /* Return null for failure */
+        return null;
+    }
+}
